6e134d6571bf86795fb4c402b5e0e9ba26c5ecdc
laura.linnenkohl <laura.linnenkohl@LAB-05121-106>
2023-04-03 15:58:14 -0400

    Fixed your math. -Order of Operations added to average -Made sure each input number is saved.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,9 +2,9 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
+float average(int n1, int n2, int n3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (n1 + n2 + n3)/3.0;
 }
 
 int main()
@@ -12,8 +12,8 @@ int main()
 	int n1 =0 , n2 = 0, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << endl<<"Second number: "; cin >> n2;
+	cout << endl<<"Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
